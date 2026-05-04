18d112cef5675e92af7cc768245845508545e68b
phuonglamhong <phuonglamhong8225@gmail.com>
2026-05-05 00:27:26 +0800

    Add config setup, base template, and user model

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
-
-from app import app, db
+from dotenv import load_dotenv
+from final_project import create_app, db
+load_dotenv()
+app = create_app()
 
 with app.app_context():
     db.create_all()
     print("Tables created.")
+
+if __name__ == "__main__":
+    app.run(debug=True)
